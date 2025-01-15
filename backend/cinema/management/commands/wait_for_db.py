361caa4d0fdf7cda28760c84420196fcd45a22e2
Yulia Pylypets <yulia250603@gmail.com>
2025-01-15 10:27:42 +0200

    Imports fixed

--- backend/cinema/management/commands/wait_for_db.py
+++ backend/cinema/management/commands/wait_for_db.py
@@ -1,7 +1,9 @@
 import time
+
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management import BaseCommand
+
 
 
 class Command(BaseCommand):
--- backend/cinema_service/urls.py
+++ backend/cinema_service/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
